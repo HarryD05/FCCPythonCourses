387a4de04e34336ba74842485182509887cea11e
Harry Davis <harrytobydavis@gmail.com>
2021-07-25 15:08:07 +0100

    Initial commit

---
+++ DataAnalysis/Exercise1Numpy.py
@@ -0,0 +1,9 @@
+# Importing libraries
+import numpy
+
+# Basic numpy arrays
+array1 = numpy.array([1, 2, 3, 4])
+array2 = numpy.array([0, 0.5, 1, 1.5, 2])
+
+print(array1.dtype)
+print(array2.dtype)
